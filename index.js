82a294ee4cb38efffc6dfdf60d3fa606c227e332
Lisa Knox <lisaknox83@gmail.com>
2023-07-04 21:23:58 -0400

    removed explicit chromium path

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const randomAdjective = require('./getAdjective');
 const { COHOST_EMAIL, COHOST_PW } = require('./SECRETS');
 
 (async () => {
-    //remove executablePath below if not on raspberry pi
-    const browser = await puppeteer.launch({ executablePath: '/usr/share/applications/chromium-browser' });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto('https://cohost.org/rc/login');
